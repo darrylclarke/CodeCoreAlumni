a9e65e1288200ac7c8880ec336f0b347fc38a097
Jeff H <jahuangi@yahoo.ca>
2015-09-19 10:39:42 -0700

    Added user association to profile, edited the form

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,5 +1,9 @@
 class Experience < ActiveRecord::Base
-  #belongs_to: :profile
+  belongs_to :user
   validates :job_title, presence: true
   validates :company, presence: true
+  validates :description, presence: true
+  validates :start_date, presence: true
+  # end_date does not need to be present. In this case, we treat this as PRESENT
+  # e.g. March 2010 - Present (present means end_date = nil)
 end

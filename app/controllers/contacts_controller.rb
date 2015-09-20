14f8394be5eb158422d4fd1174221df607c79b39
Ali Alsaihaty <ali.alsaihaty@gmail.com>
2015-09-20 16:13:48 -0700

    added mailer button functionality to show page

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,23 +1,24 @@
 class ContactsController < ApplicationController
-    
-    def new
-        @user_id = params[:user_id]
-        @user=User.find(params[:user_id])
-        @contact = Contact.new
-    end
-    
-    
-    def create
-        user=User.find(params[:user_id])
-        title = params[:contact][:title]
-        email = params[:contact][:email]
-        body  = params[:contact][:body]
-        ContactsMailer.notify_profile_owner(user, title, email, body).deliver_now
-        redirect_to root_path
-    end
-    
-    
-    def contact_params
-      params.require(:education).permit(:degree, :institution, :desc, :year)
-    end
+
+  def new
+    # @user_id = params[:user_id]
+    @user    = @profile.user
+    @contact = Contact.new
+  end
+
+
+  def create
+    user = User.find params[:user_id]
+    # user  = @profile.user
+    title = params[:contact][:title]
+    email = params[:contact][:email]
+    body  = params[:contact][:body]
+    ContactsMailer.notify_profile_owner(user, title, email, body).deliver_now
+    redirect_to root_path
+  end
+
+
+  def contact_params
+    params.require(:education).permit(:degree, :institution, :desc, :year)
+  end
 end

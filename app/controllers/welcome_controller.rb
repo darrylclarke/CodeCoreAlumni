c33027e77b71e5360929eac6aa5d1b757db3e762
Ali Alsaihaty <ali.alsaihaty@gmail.com>
2015-09-20 20:17:58 -0700

    add all students and available students

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,23 @@
 class WelcomeController < ApplicationController
+
+
   def index
+    @profiles = Profile.all
+    # if params[:search]
+    #   @profiles = Profile.search(params[:search]).Profile.order("#{params[:order]}").page(params[:page]).per(per_page)
+    # else
+    #   @profiles = Profile.order("#{params[:order]}").page(params[:page]).per(per_page)
+    # end
   end
+
+  def show
+  end
+  def all_student
+
+  end
+
+  def for_hire
+
+  end
+
 end

276f2a061057dc475e2919af938bd5043d19322c
Brian Ting <briantingtc@gmail.com>
2015-09-20 17:20:25 -0700

    Revert "Nicoehs"

--- db/migrate/20150920214051_create_skills.rb
+++
@@ -1,9 +0,0 @@
-class CreateSkills < ActiveRecord::Migration
-  def change
-    create_table :skills do |t|
-      t.references :user, index: true, foreign_key: true
-
-      t.timestamps null: false
-    end
-  end
-end

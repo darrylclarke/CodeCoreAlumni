3af48e7cdd9831f486a7daf6536519bd66fccddd
Jeff H <jahuangi@yahoo.ca>
2015-09-20 22:37:13 -0700

    added recaptcha stuff for deployment

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,6 +1,7 @@
 Recaptcha.configure do |config|
-  config.public_key  = '6LczMw0TAAAAAFit-V8YY_pAw0gP7D17ly9eQrrs'
-  config.private_key = '6LczMw0TAAAAAGcFLSfx0IOeQPPLvXjJ7vjVId_r'
+  config.public_key  = '6LeoOA0TAAAAAIGFiD2azfw4WXvA0uX8lV4V-f2v'
+
+  config.private_key = '6LeoOA0TAAAAAP-OkhnUlVDi99Ebz0tQ37gSbwOr'
   # Uncomment the following line if you are using a proxy server:
   # config.proxy = 'http://myproxy.com.au:8080'
   # Uncomment if you want to use the newer version of the API,
